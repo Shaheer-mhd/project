57d09533ad78518c6a7bba3757589a40459f69e5
shaheer <shaheermhd007@gmail.com>
2022-03-29 22:52:13 -0700

    pycharm test

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Member(models.Model):
+    Name = models.CharField(max_length=100, null=True, blank=True)
+    Place = models.CharField(max_length=100, null=True, blank=True)
+    Age = models.IntegerField(null=True, blank=True)
+    Ph = models.IntegerField(null=True, blank=True)
+    Height = models.IntegerField(null=True, blank=True)
+    Weight = models.IntegerField(null=True, blank=True)
+
+
+class Feedback(models.Model):
+    NAMAM = models.CharField(max_length=100)
+    Email = models.EmailField(max_length=50)
+    Subject = models.CharField(max_length=100)
+    Message = models.CharField(max_length=100)
--- home/views.py
+++ home/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from home.models import Member, Feedback
+
+
 def fun(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    else:
+        name1 = request.POST.get('name1')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+    Feedback.objects.create(NAMAM=name1, Email=email, Subject=subject, Message=message)
+    return redirect('home:fun')
 
 
-def gain(request):
-    return render(request, 'gain.html')
+def registration(request):
+    if request.method == 'GET':
+        return render(request, 'registration.html')
+    else:
+        name = request.POST.get('name')
+        place = request.POST.get('place')
+        age = request.POST.get('age')
+        ph = request.POST.get('ph')
+        height = request.POST.get('height')
+        weight = request.POST.get('weight')
+    Member.objects.create(Name=name, Place=place, Age=age, Ph=ph, Height=height, Weight=weight)
+    return redirect('home:fun')
